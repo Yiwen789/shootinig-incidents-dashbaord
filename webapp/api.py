1286bb704b72aa7feba9b7f8cf12872fbad2c11c
Ruofei-Li <953509678@qq.com>
2020-11-13 01:19:17 +0800

    Api.py modified to enable direct execution for testing and debugging. Add an endpoint to return all possible values of variables.

--- webapp/api.py
+++ webapp/api.py
@@ -10,19 +10,20 @@ import flask
 import json
 import config
 import psycopg2
+import argparse
 
 from config import user
 from config import password
 from config import database
 
-api = flask.Blueprint('api', __name__)
+#api = flask.Blueprint('api', __name__)
+api = flask.Flask(__name__)
 
 try:
-    connection = psycopg2.connect(database=database, user=user, password=password)
+    connection = psycopg2.connect(database=database, user=user, port='5433', password=password)
 except Exception as e:
     print("Error: problem with configuration", file=sys.stderr)
     exit()
-
 
 def send_query(query):
     try:
@@ -32,6 +33,20 @@ def send_query(query):
         print(e)
         exit()
     return cursor
+
+def dropdown_options(variable_name):
+    # return the all possible values for a specified variable
+    # Normally stored in table variable_name.type, except for booleans
+    list_to_show = []
+    column_name = 'type'
+    # print(variable_name)
+    if variable_name == 'boolean_match':
+        column_name = 'boolean'
+    query = 'SELECT ' + column_name + ' FROM '+ variable_name
+    values = send_query(query)
+    for option in values:
+        list_to_show.append(option[0]) # Note that 'option' is a list with length 1.
+    return json.dumps(list_to_show)
 
 @api.route('/cases/total')#[{filter = [mental_illness | threat_level | flee | death |arms]}]
 def hello():
@@ -76,7 +91,7 @@ def hello():
     else:
         #iterate through each selected criteria and add to the sql query
         for key in filter_variables:
-            query = f'''SELECT states.state, states.state_full_name, incidents.date, victims.full_name, gender.type, race.type, victims.age
+            query = '''SELECT states.state, states.state_full_name, incidents.date, victims.full_name, gender.type, race.type, victims.age
                        FROM incidents, locations, states, victims, gender, race
                        WHERE incidents.id = locations.id
                          AND incidents.id = victims.id
@@ -109,11 +124,29 @@ def hello():
         dic_list_to_return.append(case_dict)
     return json.dumps(dic_list_to_return)
 
-'''
+@api.route('/options/<variable_name>')
+# This returns a json list of all possible values for a specified variable. Might not be useful.
+def dropdown_options_for(variable_name):
+    table_name = ''
+    print(table_name)
+    if variable_name == 'signs_of_mental_illness' or variable_name == 'body_camera':
+        # Variables sharing the boolean values table
+        table_name = 'boolean_match'
+    elif variable_name == 'flee' or variable_name == 'threat_level' or variable_name == 'manner_of_death' or variable_name == 'arm_category':
+        # Variables have own tables
+        table_name = variable_name
+        print(table_name)
+    else:
+        pass
+    return dropdown_options(table_name)
+
+
+
+
+# TEST ------ Can run api.py directly.
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('A tiny Flask application, including API')
+    parser = argparse.ArgumentParser('Covid19_API')
     parser.add_argument('host', help='the host on which this application is running')
     parser.add_argument('port', type=int, help='the port on which this application is listening')
     arguments = parser.parse_args()
-    app.run(host=arguments.host, port=arguments.port, debug=True)
-'''
+    api.run(host=arguments.host, port=arguments.port, debug=True)
